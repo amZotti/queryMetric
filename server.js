4e0e53b20822bacd4a7cde9b7d755b3eda25b442
Anthony Zotti <azotti1337@gmail.com>
2015-04-30 16:44:12 -0700

    User can query API for metric data

--- server.js
+++ server.js
@@ -1,8 +1,24 @@
+var models = require('./db/models.js');
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+
+//exports.processRequest({target, range, date, aggregate, aggregate_target})
+
+app.get('/*', function (req, res) {
+  var params = req.params[0].split('/');
+
+  var query = {
+    filter: params[0],
+    range: params[1],
+    date: params[2],
+    aggregate: params[3],
+    aggregateTarget: params[4]
+  };
+
+  models.processRequest(query).then(function(result) {
+    res.send('Result: ' + result);
+  });
 });
 
 var server = app.listen(3000, function () {
