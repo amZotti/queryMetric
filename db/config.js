dee2f25e2dd381700164f8cfed2934b5fab020a9
Anthony Zotti <azotti1337@gmail.com>
2015-04-30 07:19:28 -0700

    Create configurations for connecting to MySQL

---
+++ db/config.js
@@ -0,0 +1,29 @@
+var mysql = require('mysql');
+
+var db_config = {
+  host: 'localhost',
+  user: 'root',
+  database: 'test'
+};
+
+handleDisconnect();
+
+function handleDisconnect() {
+  exports.connection = mysql.createConnection(db_config);
+
+  exports.connection.connect(function(err) {
+    if(err) {
+      console.log('error when connecting to db:', err);
+      setTimeout(handleDisconnect, 2000);
+    }
+  });
+
+  exports.connection.on('error', function(err) {
+    console.log('db error', err);
+    if(err.code === 'PROTOCOL_CONNECTION_LOST') {
+      handleDisconnect();
+    } else {
+      throw err;
+    }
+  });
+}
